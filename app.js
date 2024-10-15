632ae004cca853610589ba70451a12cf0b7ba5a2
VoidSplit <46133265+VoidSplit@users.noreply.github.com>
2024-10-15 10:44:33 +0200

    Update the website

---
+++ app.js
@@ -0,0 +1,514 @@
+let item_categories = [
+    //bread
+    {
+        item: "bread",
+        textures: [
+            {
+                name : "brioche",
+                display_name: "Brioche",
+                cmd: 1
+            },
+            {
+                name : "cake_au_raisin",
+                display_name: "Cake au raisin",
+                cmd: 2
+            },
+            {
+                name : "croissant",
+                display_name: "Croissant",
+                cmd: 3
+            },
+            {
+                name : "donut",
+                display_name: "Donut",
+                cmd: 4
+            },
+            {
+                name : "pain_brioche",
+                display_name: "Pain brioche",
+                cmd: 5
+            },
+            {
+                name : "pate_a_pizza",
+                display_name: "Pate a pizza",
+                cmd: 6
+            },
+            {
+                name : "brioche_aux_raisins",
+                display_name: "Brioche aux raisins",
+                cmd: 7
+            },
+            {
+                name : "brioche_tressee",
+                display_name: "Brioche tressee",
+                cmd: 8
+            },
+            {
+                name : "prefous",
+                display_name: "Prefous",
+                cmd: 9
+            },
+            {
+                name : "baguette",
+                display_name: "Baguette",
+                cmd: 10
+            },
+            {
+                name : "pain_de_mie",
+                display_name: "Pain de mie",
+                cmd: 12
+            },
+        ]
+    },
+    //glistering_melon_slice
+    {
+        item: "glistering_melon_slice",
+        textures: [
+            {
+                name : "piece_cuivre",
+                display_name: "Ecu de cuivre",
+                cmd: 1
+            },
+            {
+                name : "piece_argent",
+                display_name: "Ecu d'argent",
+                cmd: 2
+            },
+            {
+                name : "piece_or",
+                display_name: "Ecu d'or",
+                cmd: 3
+            },
+            {
+                name : "perle_brune",
+                display_name: "Perle brune",
+                cmd: 4
+            },
+            {
+                name : "perle_blonde",
+                display_name: "Perle blonde",
+                cmd: 5
+            },
+        ]
+    },
+    //knowledge_book
+    {
+        item: "knowledge_book",
+        textures: [
+            {
+                name : "huitre",
+                display_name: "Huitre",
+                cmd: 1
+            },
+            {
+                name : "huitre_vide",
+                display_name: "Huitre vide",
+                cmd: 2
+            },
+            {
+                name : "coquille_huitre",
+                display_name: "Coquille huitre",
+                cmd: 3
+            },
+            {
+                name : "marteau",
+                display_name: "Marteau",
+                cmd: 4
+            },
+            {
+                name : "cle_fer",
+                display_name: "Cle en fer",
+                cmd: 5
+            },
+            {
+                name : "cle_cuivre",
+                display_name: "Cle en cuivre",
+                cmd: 6
+            },
+            {
+                name : "cle_or",
+                display_name: "Cle en or",
+                cmd: 7
+            },
+            {
+                name : "cadena_fer",
+                display_name: "Cadena en fer",
+                cmd: 8
+            },
+            {
+                name : "cadena_cuivre",
+                display_name: "Cadena en cuivre",
+                cmd: 9
+            },
+            {
+                name : "cadena_or",
+                display_name: "Cadena en or",
+                cmd: 10
+            },
+            {
+                name : "lanterne_eteinte",
+                display_name: "Lanterne eteinte",
+                cmd: 11
+            },
+            {
+                name : "lanterne_allumee",
+                display_name: "Lanterne allumee",
+                cmd: 12
+            },
+            {
+                name : "pignon",
+                display_name: "Pignon",
+                cmd: 13
+            },
+            {
+                name : "trompette",
+                display_name: "Trompette",
+                cmd: 14
+            },
+            {
+                name : "bourse",
+                display_name: "Bourse",
+                cmd: 15
+            },
+        ]
+    },
+    //cod
+    {
+        item: "cod",
+        textures: [
+            {
+                name : "brochet",
+                display_name: "Brochet",
+                cmd: 1
+            },
+            {
+                name : "carpe",
+                display_name: "Carpe",
+                cmd: 2
+            },
+            {
+                name : "thon",
+                display_name: "Thon",
+                cmd: 3
+            },
+            {
+                name : "truite",
+                display_name: "Truite",
+                cmd: 4
+            },
+            {
+                name : "sardine",
+                display_name: "Sardine",
+                cmd: 5
+            },
+            {
+                name : "meduse",
+                display_name: "Meduse",
+                cmd: 6
+            },
+        ]
+    },
+    //snowball
+    {
+        item: "snowball",
+        textures: [
+            {
+                name : "pine",
+                display_name: "Pomme de pin",
+                cmd: 1
+            },
+        ]
+    },
+    //golden_carrot
+    {
+        item: "golden_carrot",
+        textures: [
+            {
+                name : "confiture",
+                display_name: "Confiture",
+                cmd: 1
+            },
+            {
+                name : "meule_de_fromage",
+                display_name: "Meule de fromage",
+                cmd: 2
+            },
+            {
+                name : "fromage_1",
+                display_name: "Fromage",
+                cmd: 3
+            },
+            {
+                name : "fromage_2",
+                display_name: "Emmental",
+                cmd: 4
+            },
+        ]
+    },
+    //milk_bucket
+    {
+        item: "milk_bucket",
+        textures: [
+            {
+                name : "seau_de_fromage",
+                display_name: "Seau de fromage",
+                cmd: 1
+            },
+        ]
+    },
+    //potion
+    {
+        item: "potion",
+        potion_color: "16777215",
+        textures: [
+            {
+                name : "biere",
+                display_name: "Biere",
+                cmd: 1
+            },
+            {
+                name : "bouteille_1",
+                display_name: "Bouteille",
+                cmd: 2
+            },
+            {
+                name : "bouteille_2",
+                display_name: "Bouteille",
+                cmd: 3
+            },
+            {
+                name : "bouteille_3",
+                display_name: "Bouteille",
+                cmd: 4
+            },
+            {
+                name : "bouteille_4",
+                display_name: "Bouteille",
+                cmd: 5
+            },
+            {
+                name : "bouteille_5",
+                display_name: "Bouteille",
+                cmd: 6
+            },
+            {
+                name : "cafe",
+                display_name: "Cafe",
+                cmd: 7
+            },
+            {
+                name : "capuccino",
+                display_name: "Capuccino",
+                cmd: 8
+            },
+            {
+                name : "expresso",
+                display_name: "Expresso",
+                cmd: 9
+            }
+        ]
+    },
+    //carrot
+    {
+        item: "carrot",
+        textures: [
+            {
+                name : "carotte_sauvage",
+                display_name: "Carotte sauvage",
+                cmd: 1
+            },
+            {
+                name : "tomate",
+                display_name: "Tomate",
+                cmd: 3
+            },
+            {
+                name : "cerises",
+                display_name: "Cerises",
+                cmd: 4
+            },
+            {
+                name : "cote_de_boeuf",
+                display_name: "Cote de boeuf",
+                cmd: 5
+            },
+            {
+                name : "cote_de_porc",
+                display_name: "Cote de porc",
+                cmd: 6
+            },
+            {
+                name : "citron",
+                display_name: "Citron",
+                cmd: 7
+            },
+            {
+                name : "cone_de_glace",
+                display_name: "Cone de glace",
+                cmd: 8
+            }
+        ]
+    },
+    //spider_eye
+    {
+        item: "spider_eye",
+        textures: [
+            {
+                name : "champignon_sauvage",
+                display_name: "Champignon sauvage",
+                cmd: 1
+            },
+        ]
+    },
+    // writable_book
+    {
+        item: "writable_book",
+        textures: [
+            {
+                name : "lettre_ouverte",
+                display_name: "Lettre ouverte",
+                cmd: 1
+            },
+            {
+                name : "lettre_fermee",
+                display_name: "Lettre fermee",
+                cmd: 2
+            },
+            {
+                name : "lettre_enroulee",
+                display_name: "Lettre enroulee",
+                cmd: 3
+            },
+            {
+                name : "lettre_dechiree",
+                display_name: "Lettre dechiree",
+                cmd: 4
+            },
+        ]
+    }
+]
+let flatten_categories = item_categories.map(c => c.textures.map(t => t.name)).flat()
+
+let displayed = flatten_categories
+let item_list = document.getElementById('item_list')
+let output = document.getElementById('output_input')
+
+
+let image_preview_box = document.getElementById('image_preview_box')
+let name_box = document.getElementById('name_box')
+let lore_box = document.getElementById('lore_box')
+
+let name_input = document.getElementById('name_input')
+let lore_input = document.getElementById('lore_input')
+
+let selected_item;
+
+let custom_name = ""
+let custom_lore = ""
+let custom_lore_cmd = ""
+
+let selector = "@s"
+let cmd_item = undefined
+let cmd_id = undefined
+let cmd_name = undefined
+
+item_categories.forEach(cat => {
+    cat.textures.forEach(item => {
+        let DOM = document.createElement('div')
+        DOM.classList = "item"
+        let DOM_img = document.createElement('img')
+        DOM_img.src = `./assets/items/${item.name}.png`
+        DOM.append(DOM_img)
+        item_list.append(DOM)
+
+        DOM.addEventListener('click', () => {
+            if(selected_item) {
+                selected_item.classList.remove('active')
+            }
+            DOM.classList.add('active')
+            selected_item = DOM
+
+
+            image_preview_box.src = `./assets/items/${item.name}.png`
+
+            cmd_item = cat.item
+            cmd_id = item.cmd
+            custom_name = item.display_name
+            cmd_name = item.display_name
+            update()
+        })
+        
+    })
+})
+
+
+
+output.value = ``
+
+const update = () => {
+    if(selected_item) {
+        visualizer_content.classList = "visible"
+        
+        if(custom_name == "") { custom_name = cmd_name}
+
+        name_box.textContent = custom_name
+        lore_box.textContent = custom_lore
+    
+    
+        let command = `/minecraft:give ${selector} minecraft:${cmd_item}[custom_model_data=${cmd_id},custom_name='["",{"text":"${custom_name}","italic":false}]'${custom_lore !== "" ? `,${custom_lore_cmd}` : ``}] 1`
+        output.value = command
+    }
+}
+
+name_input.addEventListener('input', (e) => {
+
+    custom_name = e.target.value
+    
+    update()
+})
+lore_input.addEventListener('input', (e) => {
+    let lines = []
+    e.target.value.split("\n").forEach(line => {
+        lines.push(`'["",{"text":"${line}","italic":false}]'`)
+    })
+    let lore_command = `lore=[${lines.join(',')}]`
+    
+    custom_lore = e.target.value.split("\n").join("\n")
+    custom_lore_cmd = lore_command
+    update()
+})
+
+let output_btn = document.getElementById('output_btn')
+let copied_popup = document.getElementById('copied_popup')
+
+let is_started = false
+const start_pop_up = () => {
+    
+    if(is_started == false) {
+        is_started = true
+        let value = 0
+        const update_popup = () => {
+            value = value + 1
+            copied_popup.setAttribute('style', `--percentage: ${value}%`)
+            if(value > 100) {
+                copied_popup.classList.remove("visible")
+                copied_popup.setAttribute('style', `--percentage: 0%`)
+                is_started = false
+                clearInterval(update_popup_interval)
+            }
+        }
+    
+        let update_popup_interval = setInterval(update_popup, 50)
+    }
+
+}
+
+
+output_btn.addEventListener('click', () => {
+    if(output.value.length > 0) {
+        copied_popup.classList.add("visible")
+        start_pop_up()
+        output.select()
+        navigator.clipboard.writeText(output.value);
+    }
+})
