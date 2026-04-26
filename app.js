06a7fc1a727fc50e89d5ffb50f353164ee7cf3f6
VoidSplit <46133265+VoidSplit@users.noreply.github.com>
2026-04-26 11:18:08 +0200

    Change command syntax for item giving

--- app.js
+++ app.js
@@ -871,7 +871,7 @@ const update = () => {
         name_box.textContent = custom_name
         lore_box.textContent = custom_lore
     
-        let command = `/give ${selector} minecraft:${base_item} ${quantity} 0 [item_model="cosmetics:${item_name}",${eatable_value === true ? `consumable={},food={nutrition:${nutrition},saturation:${saturation},can_always_eat:${infinity_value}},` : ``}${drinkable_value === true ? `consumable={animation:"drink",has_consume_particles:false,sound:"minecraft:entity.generic.drink"},` : ``}minecraft:custom_name={"text":"${custom_name}","italic":false} ${custom_lore !== "" ? `,${get_lore_cmd()}` : ``}]`
+        let command = `/minecraft:give ${selector} minecraft:${base_item} [item_model="cosmetics:${item_name}",${eatable_value === true ? `consumable={},food={nutrition:${nutrition},saturation:${saturation},can_always_eat:${infinity_value}},` : ``}${drinkable_value === true ? `consumable={animation:"drink",has_consume_particles:false,sound:"minecraft:entity.generic.drink"},` : ``}minecraft:custom_name={"text":"${custom_name}","italic":false} ${custom_lore !== "" ? `,${get_lore_cmd()}` : ``}${quantity} ]`
 
         output.value = command
     }
